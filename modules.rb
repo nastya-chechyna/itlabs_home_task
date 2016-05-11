c3123d21cef3e1343c5f0b58718a5dd5983e0d22
Anastasiia Chechyna <anastasiia.chechyna@synapse.com>
2016-05-11 15:22:35 +0300

    5/11 update

--- modules.rb
+++ modules.rb
@@ -4,12 +4,12 @@ module Modules
     @driver.navigate.to  'http://demo.redmine.org/'
     @driver.find_element(:class, 'register').click
     @wait.until {@driver.find_element(:id, 'user_login').displayed?}
-    @login = ('login' + rand(9999).to_s)
+    @login = ('test_user' + rand(9999).to_s)
     @driver.find_element(:id, 'user_login').send_keys @login
     @driver.find_element(:id, 'user_password').send_keys 'password'
     @driver.find_element(:id, 'user_password_confirmation').send_keys 'password'
-    @driver.find_element(:id, 'user_firstname').send_keys 'loginname'
-    @driver.find_element(:id, 'user_lastname').send_keys 'loginname'
+    @driver.find_element(:id, 'user_firstname').send_keys @login
+    @driver.find_element(:id, 'user_lastname').send_keys 'last_n'
     @driver.find_element(:id, 'user_mail').send_keys (@login + '@test.com')
     @driver.find_element(:name, 'commit').click
 
@@ -45,15 +45,9 @@ module Modules
     @driver.find_element(:name, 'commit').click
   end
 
-  def create_new_memeber
+  def adding_new_member
     @driver.find_element(:id, 'tab-members').click
     @driver.find_element(:class, 'icon-add').click
-    sleep(3)
-    @driver.find_element(:id, 'principal_search').send_keys 'ManagerTest'
-    @wait.until {@driver.find_element(:css, "input[value='146955']").displayed?}
-    @driver.find_element(:css, "input[value='146955']").click
-    @driver.find_element(:class, 'roles-selection').find_element(:css, "[value='3']").click
-    @driver.find_element(:id, 'member-add-submit').click
     sleep(3)
   end
 
@@ -67,7 +61,7 @@ module Modules
 
   def create_new_product_version
     @driver.find_element(:id, 'tab-versions').click
-    @driver.find_element(:id, 'tab-content-versions').find_element(:class, 'icon icon-add').click
+    @driver.find_element(:id, 'tab-content-versions').find_element(:css, "a[class='icon icon-add']").click
     @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
     @wait.until {@driver.find_element(:id, 'version_name').displayed?}
     @driver.find_element(:id, 'version_name').send_keys '1.1'
@@ -83,8 +77,9 @@ module Modules
 
   def issue_feature
     @driver.find_element(:class, 'new-issue').click
+    sleep(1)
     @driver.find_element(:id, 'issue_tracker_id').find_element(:css, "option[value='2']").click
-    sleep(3)
+    @wait.until {@driver.find_element(:css, "option[value='2']").displayed?}
     @driver.find_element(:id, 'issue_subject').send_keys 'New_issue. Feature type'
     @driver.find_element(:name, 'commit').click
   end
@@ -93,7 +88,7 @@ module Modules
     @driver.find_element(:class, 'new-issue').click
     sleep(1)
     @driver.find_element(:id, 'issue_tracker_id').find_element(:css, "option[value='3']").click
-    sleep(3)
+    @wait.until {@driver.find_element(:css, "option[value='3']").displayed?}
     @driver.find_element(:id, 'issue_subject').send_keys 'New_issue. Support type'
     @driver.find_element(:name, 'commit').click
   end
--- tests.rb
+++ tests.rb
@@ -27,6 +27,8 @@ class TestTests < Test::Unit::TestCase
   end
 
   def test_positive_log_in
+    register_user
+    log_out
     log_in
     button_logout = @driver.find_element(:class, 'my-account')
     assert(button_logout.displayed?)
@@ -49,16 +51,40 @@ class TestTests < Test::Unit::TestCase
     assert_equal(expected_text, actual_text)
   end
 
-  def test_create_new_member
+  def test_adding_new_member
+    register_user
+    name_new_member=@login
+    log_out
     register_user
     create_new_project
-    new_member=@driver.find_element(:css, "a[href='/users/146955']")
-    assert(new_member.displayed?)
+    old_member=@driver.find_elements(:css, "tr[id*='member']").count
+    adding_new_member
+    @driver.find_element(:id, 'principal_search').send_keys name_new_member
+    @wait.until{@driver.find_elements(:css, "#principals_for_new_member [name*='membership']").count == 1}
+    @driver.find_element(:css, "#principals_for_new_member [name*='membership']").click
+
+    @driver.find_element(:css, ".roles-selection [value='3']").click
+    @driver.find_element(:id, 'member-add-submit').click
+    sleep(3)
+    new_member=@driver.find_elements(:css, "tr[id*='member']").count
+    assert((new_member - old_member) ==1)
   end
 
   def test_edit_roles
     register_user
+    name_new_member=@login
+    log_out
+    register_user
     create_new_project
+    old_member=@driver.find_elements(:css, "tr[id*='member']").count
+    adding_new_member
+    @driver.find_element(:id, 'principal_search').send_keys name_new_member
+    @wait.until{@driver.find_elements(:css, "#principals_for_new_member [name*='membership']").count == 1}
+    @driver.find_element(:css, "#principals_for_new_member [name*='membership']").click
+
+    @driver.find_element(:css, ".roles-selection [value='3']").click
+    @driver.find_element(:id, 'member-add-submit').click
+    sleep(3)
     member_edit_role
     expected_text=@driver.find_element(:css, "tr[class='even member']").find_element(:class, 'hol').find_element(:css, "[value='4']")
     assert(expected_text.displayed?)
@@ -97,6 +123,7 @@ class TestTests < Test::Unit::TestCase
     create_new_project
     issue_support
     support_created=@driver.find_element(:xpath, "//div[@id='content']/*[3]").text
+    puts support_created
     assert(support_created.include?'Support')
   end
 
